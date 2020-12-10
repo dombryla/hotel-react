edafb708ae18c3117a9aa44e46fe1452172f70db
dombryla <dombryla@interia.pl>
2020-12-10 13:07:29 +0100

    Add homepage

--- code/frontend/src/pages/content/home.tsx
+++ code/frontend/src/pages/content/home.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "./home.css";
 
 export const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+        amet at ad porro doloribus quidem illum ducimus, saepe cupiditate
+        repellat mollitia laudantium, nisi distinctio ipsum voluptates cumque
+        rerum quas molestias.
+      </p>
+      <h3>You will love our hospitality</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+        amet at ad porro doloribus quidem illum ducimus, saepe cupiditate
+        repellat mollitia laudantium, nisi distinctio ipsum voluptates cumque
+        rerum quas molestias.
+      </p>
+    </div>
+  );
 };
