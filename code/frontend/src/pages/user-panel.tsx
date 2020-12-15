26d09ec538dc64e2f43e86445c2dd3b765cfc75e
dombryla <dombryla@interia.pl>
2020-12-15 23:30:24 +0100

    Refactor

--- code/frontend/src/pages/user-panel.tsx
+++ code/frontend/src/pages/user-panel.tsx
@@ -1,19 +1,15 @@
-import React, {useContext} from "react";
+import React from "react";
 import {Navigation} from "./navigation";
 import {Content} from "./content/content";
-import {UserContext} from "../userContext";
 
 import "./user-panel.css";
 
 export const UserPanel = ({logout}: any) => {
-  const {user} = useContext(UserContext);
-  console.log(user);
   return (
     <>
       <div className="userPanelContainer">
         <Navigation logout={logout} />
         <Content />
-        <div>Footer</div>
       </div>
     </>
   );
